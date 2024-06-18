5e9cc933e430da3a70fbf66e703432221028a548
Vishakh <vishakh@users.noreply.github.com>
2024-06-18 15:32:48 -0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { app, BrowserWindow } from 'electron';
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
