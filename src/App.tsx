e3082795f784b0ae88dff869325c6aefcba69e7a
Vishakh <vishakh@users.noreply.github.com>
2024-06-20 20:55:45 -0700

    Added more wallet functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
-import { unlockWallet } from './utils/WalletUtils';
+import { unlockWallet, sendTransaction } from './utils/WalletUtils';
+import { RpcClient } from '@taquito/rpc';
 
 const App: React.FC = () => {
     const [file, setFile] = useState<File | null>(null);
@@ -7,9 +8,24 @@ const App: React.FC = () => {
     const [error, setError] = useState<string | null>(null);
     const passwordRef = useRef<HTMLInputElement>(null); // Using a ref to access the password input
 
+    const [accountBalance, setAccountBalance] = useState(0);
+
+    const [recipientAddress, setRecipientAddress] = useState('');
+    const [amount, setAmount] = useState('');
+
+    const [txHash, setTxHash] = useState('');
+
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files ? event.target.files[0] : null;
         setFile(file);
+    };
+
+    const handleRecipientChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRecipientAddress(event.target.value);
+    };
+
+    const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAmount(event.target.value);
     };
 
     const handleUnlockWallet = async () => {
@@ -20,8 +36,43 @@ const App: React.FC = () => {
                 fileReader.onload = async (e) => {
                     const text = e.target?.result;
                     if (typeof text === 'string') {
-                        const address = await unlockWallet(text, password);
-                        setAddress(address);
+                        const accountInfo = await unlockWallet(text, password);
+                        setAddress(accountInfo.address);
+                    }
+                };
+                fileReader.readAsText(file);
+            } catch (error: any) {
+                setError('Failed to unlock wallet: ' + error.message);
+            }
+        } else {
+            setError('Please select a wallet file and enter a password.');
+        }
+    };
+    const handleBalanceFetch = async () => {
+        const client = new RpcClient('https://api.tez.ie/rpc/mainnet');
+        const balanceInMutez = await client.getBalance(address || '');
+        const balanceInTez = balanceInMutez.toNumber() / 1000000;
+        console.log('Balance num:', balanceInTez);
+        setAccountBalance(balanceInTez);
+    }
+
+    const handleSend = () => {
+        console.log('Sending:', amount, 'to', recipientAddress);
+        if (file && passwordRef.current && passwordRef.current.value) {
+            try {
+                const password = passwordRef.current.value; // Access the password directly from the input
+                const fileReader = new FileReader();
+                fileReader.onload = async (e) => {
+                    const text = e.target?.result;
+                    if (typeof text === 'string') {
+                        const txHash = await sendTransaction(
+                            text,
+                            password,
+                            recipientAddress,
+                            parseFloat(amount),
+                            'https://api.tez.ie/rpc/mainnet'
+                        );
+                        setTxHash(txHash)
                     }
                 };
                 fileReader.readAsText(file);
@@ -33,14 +84,43 @@ const App: React.FC = () => {
         }
     };
 
+
     return (
         <div>
-            <h1>Hello from Galleon!!!</h1>
-            <input type="file" onChange={handleFileChange} />
-            <input type="password" ref={passwordRef} placeholder="Enter password" />
+            <h1>MiniGalleon</h1>
+
+            <h2>Settings</h2>
+
+            <h2>Wallet</h2>
+            <input type="file" onChange={handleFileChange}/>
+            <input type="password" ref={passwordRef} placeholder="Enter password"/>
+
+            <h2>Unlock</h2>
             <button onClick={handleUnlockWallet}>Unlock Wallet</button>
             {address && <p>Wallet Address: {address}</p>}
             {error && <p>Error: {error}</p>}
+
+            <h2>Balance</h2>
+            <p>Balance: {accountBalance} ꜩ</p>
+            <button onClick={handleBalanceFetch}>Fetch Balance</button>
+
+            <h2>Send</h2>
+            <input
+                type="text"
+                placeholder="Recipient Address"
+                value={recipientAddress}
+                onChange={handleRecipientChange}
+            />
+            <input
+                type="number"
+                placeholder="Amount"
+                value={amount}
+                onChange={handleAmountChange}
+            />
+            <button onClick={handleSend}>Send</button>
+
+            <h2>Results</h2>
+            <p>Transaction Hash: {txHash}</p>
         </div>
     );
 };
--- src/utils/WalletUtils.ts
+++ src/utils/WalletUtils.ts
@@ -2,18 +2,26 @@ import {CryptoUtils} from 'conseiljs-softsigner';
 import {TezosMessageUtils} from "conseiljs";
 import {EncryptedWalletVersionOne, KeyStore} from "../types/WalletTypes";
 
+const { TezosToolkit } = require('@taquito/taquito');
+const { InMemorySigner } = require('@taquito/signer');
+
 import { Buffer } from 'buffer';
 window.Buffer = Buffer;
 
+export type AccountInfo= {
+    privateKey: string;
+    address: string;
+};
+
 /**
  * Unlocks a wallet using the given filename and password.
- * @param {string} fileContents - Contents of wallet file.
+ * @param {string} walletFileContents - Contents of wallet file.
  * @param {string} passphrase - Passphrase to unlock the wallet.
  * @returns {Promise<string>} The Tezos account address associated with the wallet.
  */
-export async function unlockWallet(fileContents: string, passphrase: string): Promise<string> {
+export async function unlockWallet(walletFileContents: string, passphrase: string): Promise<AccountInfo> {
     try {
-        const ew: EncryptedWalletVersionOne = JSON.parse(fileContents) as EncryptedWalletVersionOne;
+        const ew: EncryptedWalletVersionOne = JSON.parse(walletFileContents) as EncryptedWalletVersionOne;
         const encryptedKeys = TezosMessageUtils.writeBufferWithHint(ew.ciphertext);
         const salt = TezosMessageUtils.writeBufferWithHint(ew.salt);
         const decryptedMessage = await CryptoUtils.decryptMessage(encryptedKeys, passphrase, salt)
@@ -30,10 +38,48 @@ export async function unlockWallet(fileContents: string, passphrase: string): Pr
             });
         });
 
-        const result =  { identities: keys };
-        return result.identities[0].publicKeyHash;
+        const result = {
+            privateKey: keys[0].secretKey,
+            address: walletData[0].publicKeyHash
+        }
+
+        return result
     } catch (error) {
         console.error('Failed to unlock wallet:', error);
         throw error;
+    }
+}
+
+export async function sendTransaction(
+    walletFileContents: string,
+    passphrase: string,
+    recipientAddress: string,
+    amount: number,
+    tezosNodeAddress: string
+): Promise<string> {
+
+    const { privateKey } = await unlockWallet(walletFileContents, passphrase);
+
+    const tezos = new TezosToolkit(tezosNodeAddress);
+
+    tezos.setProvider({
+        signer: new InMemorySigner(privateKey)
+    });
+
+    try {
+        // Send the transaction
+        const op = await tezos.wallet.transfer({
+            to: recipientAddress,
+            amount: amount // Amount is in tez (1 tez = 1,000,000 microtez)
+        }).send();
+
+        // Confirm the transaction
+        const confirmation = await op.confirmation(1);
+        console.log(`Transaction successfully included in block: ${confirmation.blockHash}`);
+        console.log(`Operation hash: ${op.opHash}`);
+        return op.opHash;
+    } catch (error) {
+        console.error(`Error in sending tez: ${error}`);
+        throw error
     }
 }
