c7bd8433170be06876c3e8540106f7f62157d654
aeesh <adamaisha352@gmail.com>
2024-07-01 18:07:09 +0100

    Added link to Discourse for those who want to open their Ledger accounts

--- src/components/UploadWallet.tsx
+++ src/components/UploadWallet.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PassphraseInput from './PassphraseInput';
 import Button from './Button';
+import openIcon from '../assets/open.png';
 
 const UploadWallet = ({
     handleFileChange,
@@ -17,13 +18,26 @@ const UploadWallet = ({
 }) => {
     return (
         <div className='bg-sky-20 py-4 px-6 border rounded-lg'>
-        <p className='font-bold'> Open Existing Wallet </p>
+        <div className='flex flex-wrap gap-2 justify-between'>
+            <p className='font-bold'> Open Existing Wallet </p>
+            <a
+                    href={'https://discourse.cryptonomic.tech/t/accessing-a-ledger-account/510'}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className='flex gap-1 font-medium'
+                >
+                    <span>
+                        Access Ledger wallet
+                    </span>
+                    <img src={openIcon} alt='open' className='w-6 h-6' />
+            </a>
+        </div>
         <div className='flex flex-wrap items-end gap-y-2 gap-x-8'>
             <div>
             <p> Upload Wallet </p>
             <input type="file" onChange={handleFileChange} className='w-[242px]'/>
             </div>
-            <PassphraseInput value={passphrase} onChange={setPassphrase} />
+            {/* <PassphraseInput value={passphrase} onChange={setPassphrase} /> */}
             <Button text={'Open'} onButtonClick={unlockWallet} disabled={disabled} />
         </div>
         </div>
