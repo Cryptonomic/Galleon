f61d239c5541464f726b50a6544acfcc9c2206e7
aeesh <adamaisha352@gmail.com>
2024-07-02 11:44:22 +0100

    modified Footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,12 @@ const Footer = () => {
     return (
         <div>
             <p className='text-xs font-light pt-6 pb-10 text-center'> {`[帆船ウォレット]`} </p>
-            <div className='flex flex-wrap justify-center gap-4'>
+            <div className='flex flex-wrap justify-center items-center gap-4'>
                 <p className='text-black text-sm font-light'> Copyright 2024, Cryptonomic Inc. </p>
-                <a href={'#'} target='_blank' rel='noopener noreferrer' className='text-sky-30 text-xs font-light'>
+                <a href={'#'} target='_blank' rel='noopener noreferrer' className='text-sky-30 text-xs'>
                     Terms of Service
                 </a>
-                <a href={'#'} target='_blank' rel='noopener noreferrer' className='text-sky-30 text-xs font-light'>
+                <a href={'#'} target='_blank' rel='noopener noreferrer' className='text-sky-30 text-xs'>
                     Privacy Policy
                 </a>
             </div>
